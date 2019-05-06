594a2639bcf6eb52624bd51a82889cb7b8933a2a
Kyle Olszewski <olszewski.kyle@gmail.com>
2019-05-06 15:05:54 -0700

    add train/test scripts

---
+++ scripts/test_nvs_car.sh
@@ -0,0 +1,1 @@
+python test_tbn.py --dataset_name car --input_model_file models/nvs_car.pth --upsample_output True --num_combine_views 4
---
+++ scripts/train_nvs_car.sh
@@ -0,0 +1,1 @@
+python train_tbn.py --dataset_name car --model_path output/nvs_car.pth --num_combine_views 4
