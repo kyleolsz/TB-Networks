5eaee284fa045bcb6980b18aa41fa2cc607e08b5
Kyle Olszewski <olszewski.kyle@gmail.com>
2019-11-01 00:13:21 +0900

    don't use discriminator when testing

--- scripts/test_nvs_chair.sh
+++ scripts/test_nvs_chair.sh
@@ -1,1 +1,1 @@
-python test_tbn.py --dataset_name chair --input_model_file models/nvs_chair.pth --upsample_output True --num_combine_views 4
+python test_tbn.py --dataset_name chair --input_model_file models/nvs_chair.pth --upsample_output True --num_combine_views 4 --use_gan False
